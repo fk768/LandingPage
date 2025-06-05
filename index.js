0ff667b40e3baedcdf962a5188fe83443012063a
Felipe Kewton <kewton.fksc@gmail.com>
2025-06-05 22:21:46 +0000

    Desisto

--- index.js
+++ index.js
@@ -4,26 +4,9 @@ const conexao = require('./DB');
 
 app.use(express.json());
 
-app.post('/verificar', (req, res) => {
-  const { id, nome, email, senha } = req.body;
-
-  const sql = 'SELECT * FROM usuarios WHERE id = ? AND nome = ? AND email = ? AND senha = ?';
-  const valores = [id, nome, email, senha];
-
-  conexao.query(sql, valores, (erro, resultados) => {
-    if (erro) {
-      res.status(500).send('Erro na consulta ao banco');
-      return;
-    }
-
-    if (resultados.length > 0) {
-      res.send('Usuário encontrado e verificado');
-    } else {
-      res.send('Usuário não encontrado ou dados incorretos');
-    }
-  });
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando!');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
-});
+app.listen(3306, () => {
+    console.log('Servidor rodando em http://localhost:3306')});
